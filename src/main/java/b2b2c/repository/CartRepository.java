ec522565049a17a4a61b02eea649d5cc7fbbd70a
Hoang Vu <hoang1791997@gmail.com>
2019-05-22 15:50:26 +0700

    navite query JPA

--- src/main/java/b2b2c/repository/CartRepository.java
+++ src/main/java/b2b2c/repository/CartRepository.java
@@ -2,7 +2,10 @@ package b2b2c.repository;
 
 import java.util.ArrayList;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import b2b2c.model.CartModel;
@@ -14,5 +17,8 @@ public interface CartRepository extends JpaRepository<CartModel, Integer> {
 	CartModel getByCartPaintIdAndCartOrderId(int cartPaintId, int cartOrderId);
 	
 	ArrayList<CartModel> getCartByCartOrderId(int cartOrderId);
+	
+//	@Query(value="SELECT * FROM tblcart WHERE cart_coupon_id=:cart_coupon_id", nativeQuery = true)
+//	CartModel layBangCoupon(@Param("cart_coupon_id") int cartCouponId);
 	
 }
